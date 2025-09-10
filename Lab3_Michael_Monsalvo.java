20f00a5c6245c76bc8a8a8bcf0a98848fa0496e5
mmons09 <mmons09@jaguar.tamu.edu>
2025-09-10 01:16:56 +0000

    Initial Save

---
+++ Lab3_Michael_Monsalvo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class InClass3_Michael_Monsalvo
+{
+
+Scanner input = new Scanner(System.in); 
+//Initial Value
+Inital Value = input.next.Double 
+System.out.println("Initial Value") 
+Scanner input = new Scanner(System.in); 
+//NumberofYears
+System.out.println("NumberofYears")
+Scanner input = new Scanner(System.in); 
+//Intrest rate
+System.out.println("Interest rate")
+}
